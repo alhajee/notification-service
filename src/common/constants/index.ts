64fc3b9b0bbfa9f3fed460dad28f62add7516b41
Muhammad Alhajee <alhajee2009@gmail.com>
2023-12-16 17:21:22 +0100

    feat: constants

---
+++ src/common/constants/index.ts
@@ -0,0 +1,14 @@
+export const NEW_TRANSACTION = 'NEW_TRANSACTION';
+export const CONTACT_EMAIL = 'CONTACT_EMAIL';
+export const SITE_URL = 'SITE_URL';
+export const MAIL_FROM = 'MAIL_FROM';
+export const MAIL_HOST = 'MAIL_HOST';
+export const MAIL_PASSWORD = 'MAIL_PASSWORD';
+export const MAIL_PORT = 'MAIL_PORT';
+export const MAIL_USER = 'MAIL_USER';
+
+export const FCM_PROJECT_ID = 'FCM_PROJECT_ID';
+export const FCM_CLIENT_EMAIL = 'FCM_CLIENT_EMAIL';
+export const FCM_PRIVATE_KEY = 'FCM_PRIVATE_KEY';
+export const HTTP_TIMEOUT = 'HTTP_TIMEOUT';
+export const HTTP_MAX_REDIRECTS = 'HTTP_MAX_REDIRECTS';
